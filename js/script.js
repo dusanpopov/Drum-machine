5dc41d60f6c4ac294d186960e1a920f7882be76e
Dušan Popov <aristocut90@gmail.com>
2021-02-03 04:47:48 +0100

    updated/changed syntax

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-function playSound(e) {
+const pads = document.querySelectorAll(".pad");
 
+const playSound = (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
     const pad = document.querySelector(`.pad[data-key="${e.keyCode}"`);
+
     if (!audio) return; // stop the function
-
     audio.currentTime = 0; // allows sound to be played over and over again
-
     audio.play();
     pad.classList.add("playing");
 }
-
 
 function removeTransition(e) {
 
@@ -17,10 +16,9 @@ function removeTransition(e) {
     this.classList.remove("playing");
 }
 
-const pads = document.querySelectorAll(".pad");
-
 pads.forEach(pad => {
     pad.addEventListener("transitionend", removeTransition);
 });
 
 window.addEventListener("keydown", playSound);
+
